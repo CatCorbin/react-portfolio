e29c10d158aabba872473c7045f93f806874757f
unknown <catcorbin0919@gmail.com>
2023-12-01 19:19:51 -0700

    resume.js minus resume

---
+++ my-portfolio/src/components/Resume.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Resume = () => {
+  return (
+    <section>
+      <h2>Resume</h2>
+      <p>
+        Download my resume:{' '}
+        <a href="/resume" target="_blank" rel="noopener noreferrer">
+          Resume.pdf
+        </a>
+      </p>
+      <h3>Technical Proficiencies</h3>
+      <ul>
+        <li>JavaScript (ES6+)</li>
+        <li>React</li>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>MongoDB</li>
+        <li>MySQL & NoSQL</li>
+        <li>Node</li>
+        <li>Express</li>
+      </ul>
+    </section>
+  );
+};
+
+export default Resume;
