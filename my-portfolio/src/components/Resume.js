b35ca4d61f90c4e63a755354cc8ae2a8c7cac8d5
unknown <catcorbin0919@gmail.com>
2023-12-01 20:45:08 -0700

    added resume

--- my-portfolio/src/components/Resume.js
+++ my-portfolio/src/components/Resume.js
@@ -6,7 +6,7 @@ const Resume = () => {
       <h2>Resume</h2>
       <p>
         Download my resume:{' '}
-        <a href="/resume" target="_blank" rel="noopener noreferrer">
+        <a href="/Resume.pdf" target="_blank" rel="noopener noreferrer">
           Resume.pdf
         </a>
       </p>
