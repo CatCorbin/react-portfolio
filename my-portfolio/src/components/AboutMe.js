661f3060d5cd3cd37bbd5b5a27a4d1c5c651138f
unknown <catcorbin0919@gmail.com>
2023-12-01 18:13:31 -0700

    filled out about me

---
+++ my-portfolio/src/components/AboutMe.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const AboutMe = () => {
+  return (
+    <section>
+      <h2>About Me</h2>
+      <div>
+        <img src="../assets/heashot.png" alt="Cat Corbin" />
+        <p>
+          Hi there! My name is Caybree Corbin, but I go by Cat! I am a new web developer, having recently completed the Fullstack Web Development Coding Bootcamp through the University of Utah. I am very passionate and eager to learn.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default AboutMe;
