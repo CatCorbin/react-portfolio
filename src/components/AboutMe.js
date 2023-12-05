260200ed1c967403fdcc47ced632184d055ef2cc
unknown <catcorbin0919@gmail.com>
2023-12-04 19:15:42 -0700

    moved pics

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -5,7 +5,7 @@ const AboutMe = () => {
     <section>
       <h2>About Me</h2>
       <div>
-        <img src="../assets/heashot.png" alt="Cat Corbin" />
+        <img src="heashot.png" alt="Cat Corbin" />
         <p>
           Hi there! My name is Caybree Corbin, but I go by Cat! I am a new web developer, having recently completed the Fullstack Web Development Coding Bootcamp through the University of Utah. I am very passionate and eager to learn.
         </p>
