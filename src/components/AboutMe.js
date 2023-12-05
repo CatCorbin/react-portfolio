59c2330a4fa93c6c988251199c5332cab3321be2
unknown <catcorbin0919@gmail.com>
2023-12-04 19:51:37 -0700

    fixed the way I was importing and calling my about me picture

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import myImage from '../images/headshot.png';
 
 const AboutMe = () => {
   return (
     <section>
       <h2>About Me</h2>
       <div>
-        <img src="heashot.png" alt="Cat Corbin" />
+      <img src={myImage} alt="Cat Corbin" />
         <p>
           Hi there! My name is Caybree Corbin, but I go by Cat! I am a new web developer, having recently completed the Fullstack Web Development Coding Bootcamp through the University of Utah. I am very passionate and eager to learn.
         </p>
