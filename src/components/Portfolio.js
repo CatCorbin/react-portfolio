689a55884da206ac0178a2a2455a2463555f7620
unknown <catcorbin0919@gmail.com>
2023-12-11 01:48:20 -0700

    added background color and button color

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,46 +1,52 @@
 import React from 'react';
+import socialMediaShowdown from '../images/sms.png';
+import noteTaker from '../images/nt.png';
+import terse from '../images/terse.png';
+import passwordGenerator from '../images/pg.png';
+import dayScheduler from '../images/ds.png';
+import portfolio from '../images/p.png';
 
 const Portfolio = () => {
   const projects = [
     {
       title: 'Social Media Showdown',
       description: 'Musk vs Zuckerberg site with a coutndown timer to their alleged fight, voting for who you think will win, and stock and social media info',
-      imageSrc: 'sms.png',
+      imageSrc:{socialMediaShowdown},
       deployedLink: 'https://c-hickman23.github.io/Musk-Vs.-Zuck/',
       githubLink: 'https://github.com/CatCorbin/social-media-showdown',
     },
     {
       title: 'Note Taker',
       description: 'A place to take notes, but more fun',
-      imageSrc: 'nt.png',
+      imageSrc:{noteTaker},
       deployedLink: 'https://catcorbin-notetaker-85973421238a.herokuapp.com/',
       githubLink: 'https://github.com/CatCorbin/note-taker',
     },
     {
         title: 'Terse',
         description: 'Social media site that allows posts, comments, DMs, and group chats, but you are only allowed to send one word',
-        imageSrc: 'terse.png',
+        imageSrc:{terse},
         deployedLink: 'https://tersesocialmedia-production.up.railway.app/',
         githubLink: 'https://github.com/CatCorbin/terse',
       },
       {
         title: 'Password Generator',
         description: 'An application that allows you to give it the criteria, and it will generate you a random password.',
-        imageSrc: 'pg.png',
+        imageSrc:{passwordGenerator},
         deployedLink: 'https://catcorbin.github.io/password-generator/',
         githubLink: 'https://github.com/CatCorbin/password-generator',
       },
       {
         title: 'Day Scheduler',
         description: 'An hour by hour calendar that you can use to schedule out your day',
-        imageSrc: 'ds.png',
+        imageSrc:{dayScheduler},
         deployedLink: 'https://catcorbin.github.io/day-scheduler/',
         githubLink: 'https://github.com/CatCorbin/day-scheduler',
       },
       {
         title: 'Portfolio',
         description: 'The first portfolio I made with HTML & CSS, a stark contrast to this portfolio made with React',
-        imageSrc: 'p.png',
+        imageSrc:{portfolio},
         deployedLink: 'https://catcorbin.github.io/Cat-Corbin-Portfolio/',
         githubLink: 'https://github.com/CatCorbin/Cat-Corbin-Portfolio',
       },
